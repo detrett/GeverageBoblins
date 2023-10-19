2c812fcc1c84c36db27551bf68ae927b71a958d3
tryma <s364596@oslomet.no>
2023-10-19 16:07:08 +0200

    Now you should return to the right page when log out.

--- GeverageBoblins/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ GeverageBoblins/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel;
 
 namespace GeverageBoblins.Areas.Identity.Pages.Account
 {
@@ -30,13 +31,13 @@ namespace GeverageBoblins.Areas.Identity.Pages.Account
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return LocalRedirect("~/Forum/Container");
             }
             else
             {
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToPage("~/Forum/Container");
             }
         }
     }
